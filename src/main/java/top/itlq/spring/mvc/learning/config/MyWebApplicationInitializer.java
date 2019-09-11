0aaaaf1f8adc350c7c7a076b13cae13775cdc18e
Qiang Lee <1725055744@qq.com>
2019-09-11 19:36:34 +0800

    start

---
+++ src/main/java/top/itlq/spring/mvc/learning/config/MyWebApplicationInitializer.java
@@ -0,0 +1,20 @@
+package top.itlq.spring.mvc.learning.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * 自动被servlet 容器检测到， 无需spring配置等
+ * 可用来代替 web.xml 配置 web 应用
+ * spring boot不同，不将其与servlet容器的连接交给开发者，而是使用spring配置引导自己和内嵌的servlet容器
+ */
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // 可在此配置 spring context，dispatcherServlet；
+        System.out.println(servletContext);
+    }
+}
