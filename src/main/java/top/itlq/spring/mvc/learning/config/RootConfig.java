7ea389c8a2d52981581969eb576473ab58922fde
Qiang Lee <1725055744@qq.com>
2019-09-17 19:59:16 +0800

    config dispatcherServlet

---
+++ src/main/java/top/itlq/spring/mvc/learning/config/RootConfig.java
@@ -0,0 +1,10 @@
+package top.itlq.spring.mvc.learning.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liqiang
+ */
+@Configuration
+public class RootConfig {
+}
